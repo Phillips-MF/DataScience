8ffca6f778f03d318b426d2e7493901b495c1c45
Phillips-MF <martinsfelipe62@gmail.com>
2025-05-15 09:34:45 -0300

    Finished my Apriori code

--- Python/Salary Linear Regression/Salary_prediction.py
+++ Python/Salary Linear Regression/Salary_prediction.py
@@ -5,16 +5,16 @@ from keras.models import Sequential
 from keras.layers import Dense ,Dropout
 from sklearn.metrics import mean_absolute_error
 
-salary_data = pd.read_csv("Data/Salary_dataset.csv")
+salary_data = pd.read_csv("C:/Users/matin/Documents/My Workshop/Python/Salary Linear Regression/Data/Salary_dataset.csv")
 salary_data.drop("Unnamed: 0", axis = 1, inplace = True)
 years_experience = salary_data.iloc[:,0]
 salary = salary_data.iloc[:,1]
 
-sns.histplot(data = salary_data, x = salary_data.iloc[:, 1])
+histplot =sns.histplot(data = salary_data, x = salary_data.iloc[:, 1])
 
-sns.scatterplot(data = salary_data, x = salary, y = years_experience)
+scatterplot = sns.scatterplot(data = salary_data, x = salary, y = years_experience)
 
-yrs_experience_training, yrs_experience_test, salary_training, salary_test = train_test_split(years_experience, salary, test_size = 0.5)
+yrs_experience_training, yrs_experience_test, salary_training, salary_test = train_test_split(years_experience, salary, test_size = 0.6)
 
 salary_predictor = Sequential() # Neural Network
 #Input Layer
@@ -29,12 +29,22 @@ salary_predictor.add(Dropout(0.1))
 #Output layer
 salary_predictor.add(Dense(units = 1, activation = 'relu'))
 #Compilation
-salary_predictor.compile(loss = 'mean_absolute_error', optimizer = 'adam', metrics = ['mean_absolute_error'])
+salary_predictor.compile(loss = 'mean_squared_error', optimizer = 'adam', metrics = ['mean_squared_error'])
 #Training session
-salary_predictor.fit(yrs_experience_training, salary_training, batch_size=10, epochs = 250)
+salary_predictor.fit(yrs_experience_training, salary_training, batch_size=10, epochs = 500)
 
 salary_prediction = salary_predictor.predict(yrs_experience_test)
 
-#Error
-mae = mean_absolute_error(salary_test, salary_prediction)
-print(round(mae))
+
+salary_prediction[0]
+salary_test[0]
+salary_prediction = pd.DataFrame({'Prediction' : salary_prediction[0]})
+salary_test = pd.DataFrame({'Test' : salary_test["Salary"]})
+combined = pd.concat([salary_prediction, salary_test], ignore_index=True)
+
+#Adcionar grafico com os valores reais e os valores previstos.
+sns.scatterplot(x = "Test", y = "Prediction", data = combined)
+salary_prediction = pd.DataFrame(salary_prediction)
+salary_prediction.to_csv("salary_prediction.csv", index = False)
+salary_test = pd.DataFrame(salary_test)
+salary_test.to_csv("salary_test.csv", index = False)
